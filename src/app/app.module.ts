502bbd430e625ee4cb25a46447d58d25986ca7f2
souravmondal13 <souravcristiano502@gmail.com>
2019-08-07 15:18:51 +0530

    added sidenav and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { MaterialModule } from './material/material.module';
+import { SidenavComponent } from './common/sidenav/sidenav.component';
+import { FooterComponent } from './common/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidenavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
