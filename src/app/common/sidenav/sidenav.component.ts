870e4693d9a0c7cc3eb0a997d3e9e77b10359b47
souravmondal13 <souravcristiano502@gmail.com>
2019-08-09 13:54:26 +0530

    created basic structure

--- src/app/common/sidenav/sidenav.component.ts
+++ src/app/common/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Isidenav } from '../model/isidenav';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -7,9 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  @Output() sidenavToggle = new EventEmitter();
+  sidenavOpen: boolean = true;
+  selectedNavItem: string;
+
+  sidenavData0: Isidenav[] = [
+    {
+      displayName: "Dashboard",
+      iconClass: "view_quilt",
+      redirectLink: "/dashboard"
+    }
+];
+
+  sidenavData: Isidenav[] = [
+    {
+      displayName: "Organization",
+      iconClass: "view_quilt",
+      redirectLink: "/organization"
+    }
+];
+  sidenavData1: Isidenav[]=[
+    {
+      displayName: "Project Office",
+      iconClass:"view_quilt",
+      redirectLink: "/project-office"
+    }
+  ];
+  sidenavData2: Isidenav[]=[
+    {
+      displayName: "Sales",
+      iconClass:"view_quilt",
+      redirectLink: "/sales"
+    }
+  ];
+  sidenavData3: Isidenav[]=[
+    {
+      displayName: "Budget",
+      iconClass:"view_quilt",
+      redirectLink: "/budget"
+    }
+  ];
+  sidenavData4: Isidenav[]=[
+    {
+      displayName: "Specification",
+      iconClass:"view_quilt",
+      redirectLink: "/specification"
+    }
+  ];
+  sidenavData5: Isidenav[]=[
+    {
+      displayName: "Inventory Management",
+      iconClass:"view_quilt",
+      redirectLink: "/inventory-management"
+    }
+  ];
+  sidenavData6: Isidenav[]=[
+    {
+      displayName: "Purchase Ofiice",
+      iconClass:"view_quilt",
+      redirectLink: "/purchase-office"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSidenavToggle() {
+    this.sidenavOpen = !this.sidenavOpen;
+    this.sidenavToggle.emit();
   }
 
 }
