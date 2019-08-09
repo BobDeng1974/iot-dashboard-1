72efaadf107a660bc6cdd3602e9b0788f42d6a91
souravmondal13 <souravcristiano502@gmail.com>
2019-08-09 15:50:16 +0530

    added shrunk open animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('sidenavState', [
+      state('open', style({
+        flex:"0 1 calc(18% - 5px)"
+      })),
+      state('shrunk', style({
+        flex:"0 1 calc(4% - 5px)",
+        fontSize: "0px"
+      })),
+      transition('shrunk => open', animate('500ms ease-in')),
+      transition('open => shrunk', animate('500ms ease-out'))
+    ]),
+    trigger('mainContentState', [
+      state('shrunk', style({
+        flex:"0 1 calc(96% - 5px)"
+      })),
+      state('open', style({
+        flex:"0 1 calc(82% -5px)"
+      })),
+      transition('shrunk => open', animate('500ms ease-in')),
+      transition('open => shrunk', animate('500ms ease-out'))
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'iot-dashboard';
+  sidenavState: string = 'open';
+  shrinkSidenav(){
+    this.sidenavState = this.sidenavState === 'open' ? 'shrunk' : 'open';
+  }
 }
