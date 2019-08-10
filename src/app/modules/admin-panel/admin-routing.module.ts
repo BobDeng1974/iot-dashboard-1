ce7ac9d32fd80d7958387d013fe664b3a7f273e8
souravmondal13 <souravcristiano502@gmail.com>
2019-08-10 14:01:04 +0530

    modified

--- src/app/modules/admin-panel/admin-routing.module.ts
+++ src/app/modules/admin-panel/admin-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminViewComponent } from './components/admin-view/admin-view.component';
+import { AdminMainComponent } from './pages/admin-main/admin-main.component';
 
 const routes: Routes = [
-  { path:"admin-panel", component:AdminViewComponent }
+  { path:"admin-panel", component:AdminMainComponent }
 ]
 
 @NgModule({
