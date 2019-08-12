435a75a51c634e8f022e4a824537fb119f275c9c
shatanu1998a <shantanurong44@gmail.com>
2019-08-12 17:48:26 +0530

    addd vendor details

--- src/app/modules/admin-panel/admin-panel.module.ts
+++ src/app/modules/admin-panel/admin-panel.module.ts
@@ -5,11 +5,12 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminViewComponent } from './components/admin-view/admin-view.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { VendorTableComponent } from './components/vendor-table/vendor-table.component';
+import { VendorDetailsComponent } from './components/vendor-details/vendor-details.component';
 
 
 
 @NgModule({
-  declarations: [AdminMainComponent, AdminViewComponent, VendorTableComponent],
+  declarations: [AdminMainComponent, AdminViewComponent, VendorTableComponent, VendorDetailsComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
