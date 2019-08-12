6607defe306a029fc2c54483f323b81a3fad0c99
shatanu1998a <shantanurong44@gmail.com>
2019-08-12 16:15:43 +0530

    add vendor table

--- src/app/modules/admin-panel/admin-panel.module.ts
+++ src/app/modules/admin-panel/admin-panel.module.ts
@@ -4,11 +4,12 @@ import { AdminMainComponent } from './pages/admin-main/admin-main.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminViewComponent } from './components/admin-view/admin-view.component';
 import { MaterialModule } from 'src/app/material/material.module';
+import { VendorTableComponent } from './components/vendor-table/vendor-table.component';
 
 
 
 @NgModule({
-  declarations: [AdminMainComponent, AdminViewComponent],
+  declarations: [AdminMainComponent, AdminViewComponent, VendorTableComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
---
+++ src/app/modules/admin-panel/components/vendor-table/vendor-table.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface PeriodicElement {
+  vendor_code: string;
+  vendor_name: string;
+  vendor_type: string;
+  vendor_tag: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {vendor_code: "001", vendor_name : "Qubematics", vendor_type : "Private", vendor_tag : "ven001"},
+  {vendor_code: "001", vendor_name : "Qubematics", vendor_type : "Private", vendor_tag : "ven001"},
+  {vendor_code: "001", vendor_name : "Qubematics", vendor_type : "Private", vendor_tag : "ven001"},
+  {vendor_code: "001", vendor_name : "Qubematics", vendor_type : "Private", vendor_tag : "ven001"},
+  {vendor_code: "001", vendor_name : "Qubematics", vendor_type : "Private", vendor_tag : "ven001"}
+];
+
+@Component({
+  selector: 'app-vendor-table',
+  templateUrl: './vendor-table.component.html',
+  styleUrls: ['./vendor-table.component.scss']
+})
+export class VendorTableComponent implements OnInit {
+
+  displayedColumns: string[] = ['select', 'vendor_code', 'vendor_name', 'vendor_type', 'vendor_tag'];
+  dataSource = ELEMENT_DATA;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
