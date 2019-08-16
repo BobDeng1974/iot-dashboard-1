cdab666416d37b1b1d12d0069214700a4456eba3
souravmondal13 <souravcristiano502@gmail.com>
2019-08-16 21:03:17 +0530

    added columnDefs

--- src/app/modules/admin-panel/components/device-detail/device-detail.component.ts
+++ src/app/modules/admin-panel/components/device-detail/device-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Device } from '../../model/customermodel';
+import { Device, Sensor } from '../../model/customermodel';
 
 @Component({
   selector: 'app-device-detail',
@@ -8,9 +8,28 @@ import { Device } from '../../model/customermodel';
 })
 export class DeviceDetailComponent implements OnInit {
   @Input() device: Device;
+  private sensors: Sensor[] = [];
+  private sensorColumnDefs;
+  private sensorGridApi;
+  private sensorGridColumnApi;
   constructor() { }
 
   ngOnInit() {
+    this.sensorColumnDefs = [
+      {headerName:'Sensor Name', field:'sensor_name'},
+      {headerName:'Sensor Type', field:'sensor_type', resizable:true},
+      {headerName:'Sensor Threshold Max', field:'sensor_threshold_max', editable:true, sortable:true},
+      {headerName:'Sensor Threshold Min', field:'sensor_threshold_min', editable:true, sortable:true},
+    ]
+  }
+  ngOnChanges(){
+    if (this.device && this.device.device_id != 0) {
+      this.sensors = this.device.sensors
+    }
+  }
+  onSensorGridReady(params){
+    this.sensorGridApi = params.api;
+    this.sensorGridColumnApi = params.columnApi
   }
 
 }
