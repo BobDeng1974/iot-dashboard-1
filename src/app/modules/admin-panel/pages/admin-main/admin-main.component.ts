bdee2d082ae431c1d824e37ee444e40dde2c8be7
souravmondal13 <souravcristiano502@gmail.com>
2019-08-17 04:46:45 +0530

    added device assign and sensor assign form and functionality

--- src/app/modules/admin-panel/pages/admin-main/admin-main.component.ts
+++ src/app/modules/admin-panel/pages/admin-main/admin-main.component.ts
@@ -19,6 +19,7 @@ import { Device, Address, Customer, Domaindata } from '../../model/customermodel
 import { AdminPanelMainService } from '../../admin-panel-main.service';
 import { SuccessSnackberComponent } from 'src/app/modules/shared/components/success-snackber/success-snackber.component';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AddSensorFormComponent } from '../../components/add-sensor-form/add-sensor-form.component';
 
 @Component({
   selector: 'app-admin-main',
@@ -71,6 +72,9 @@ export class AdminMainComponent implements OnInit {
 
   //device assign dialog
   deviceAssignDialog : MatDialogRef<AddDeviceComponent>
+
+  //sensor assign dialog
+  sensorAssignDialog: MatDialogRef<AddSensorFormComponent>
 
   selectedTab = 0;
 
@@ -144,7 +148,7 @@ export class AdminMainComponent implements OnInit {
     this.adminpanelService.getAllDevice().subscribe(
       (data) => {
         console.log(data)
-        this.deviceData = data
+        this.deviceData = data.reverse()
       },
       (error) => {
         console.log(error)
@@ -380,6 +384,21 @@ export class AdminMainComponent implements OnInit {
       //open add device dialog 
       case 16:
         this.deviceAssignDialog = this.dialog.open(AddDeviceComponent)
+        this.deviceAssignDialog.afterClosed().subscribe(result => {
+          if (result) {
+            this.spinner.show()
+            this.adminpanelService.getAllDevice().subscribe(
+              (data) => {
+                this.deviceData = data.reverse();
+                this.spinner.hide()
+              },
+              (error) => {
+                console.log(error);
+                this.spinner.hide()
+              }
+            );
+          }
+        });
       break;
 
       //open edit device form
@@ -387,6 +406,31 @@ export class AdminMainComponent implements OnInit {
         this.deviceAssignDialog = this.dialog.open(AddDeviceComponent, {
           data: this.deviceDetail
         })
+      break;
+
+      //open sensor form
+      case 18:
+        this.sensorAssignDialog = this.dialog.open(AddSensorFormComponent)
+        this.sensorAssignDialog.afterClosed().subscribe(result => {
+          if (result) {
+            this.deviceDetail.sensors.push(result);
+            this.spinner.show()
+            this.adminpanelService.updateDevice(this.deviceDetail).subscribe(
+              (data) => {
+                if (data == "001") {
+                  alert('updated successfully')
+                }else{
+                  console.log('error occured')
+                }
+                this.spinner.hide()
+              },
+              (error) => {
+                console.log(error)
+                this.spinner.hide()
+              }
+            );
+          }
+        });
       break;
     }
   }
