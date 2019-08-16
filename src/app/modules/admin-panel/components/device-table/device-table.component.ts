b820376d042f0a1ef9e797bb4a56aea064a8eeba
souravmondal13 <souravcristiano502@gmail.com>
2019-08-16 18:43:15 +0530

    changed dataSource

--- src/app/modules/admin-panel/components/device-table/device-table.component.ts
+++ src/app/modules/admin-panel/components/device-table/device-table.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Customer, Device } from '../../model/customermodel';
+import { EventManager } from '@angular/platform-browser';
 
-export interface PeriodicElement {
-  device_name : string;
-  device_mac_address : string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {device_name : "device1", device_mac_address : "00123"},
-  {device_name : "device1", device_mac_address : "00123"},
-  {device_name : "device1", device_mac_address : "00123"},
-  {device_name : "device1", device_mac_address : "00123"},
-  {device_name : "device1", device_mac_address : "00123"},
-  {device_name : "device1", device_mac_address : "00123"},
-  {device_name : "device1", device_mac_address : "00123"}
+const ELEMENT_DATA: Device[] = [
+  {device_id:1, device_name : "device1", device_mac: "00123"},
+  {device_id:2, device_name : "device1", device_mac: "00123"},
+  {device_id:3, device_name : "device1", device_mac: "00123"},
+  {device_id:4, device_name : "device1", device_mac: "00123"},
+  {device_id:5, device_name : "device1", device_mac: "00123"},
+  {device_id:6, device_name : "device1", device_mac: "00123"},
+  {device_id:7, device_name : "device1", device_mac: "00123"}
 ];
 
 @Component({
@@ -21,12 +19,40 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./device-table.component.scss']
 })
 export class DeviceTableComponent implements OnInit {
+  @Output() buttonClicked = new EventEmitter<number>();
+  @Output() deviceDetails = new EventEmitter<Device>();
+  @Output() deviceName = new EventEmitter<string>();
+  @Output() deviceName2 = new EventEmitter<string>();
+  @Output() deviceId = new EventEmitter<number>();
+  @Output() deviceId2 = new EventEmitter<number>();
+  @Output() deviceAssign = new EventEmitter<number>();
 
-  displayedColumns: string[] =['select', 'device_name', 'device_mac_address'];
+  displayedColumns: string[] =['select', 'device_name', 'device_mac', 'device_monitor', 'device_assignment'];
   dataSource = ELEMENT_DATA;
+  selectedDevice: Device = {
+    device_id: 1
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+  InitializeClick(value:number){
+    this.buttonClicked.emit(value)
+  }
+
+  gotoDeviceMonitor(rowId: number, name: string){
+    this.deviceName2.emit(name)
+    this.deviceId2.emit(rowId);
+    console.log(rowId)
+  }
+
+  gotoDeviceAssignment(rowId: number, name: string){
+    this.deviceName.emit(name)
+    this.deviceAssign.emit(rowId)
+  }
+
+  viewDetails(value: Device){
+    this.deviceDetails.emit(value)
+  }
 }
