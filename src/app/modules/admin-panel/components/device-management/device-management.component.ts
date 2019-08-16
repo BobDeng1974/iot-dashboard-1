ada873dc7c93a6618e791f7ff6d61cfd82aa4a49
souravmondal13 <souravcristiano502@gmail.com>
2019-08-16 18:38:37 +0530

    created components

---
+++ src/app/modules/admin-panel/components/device-management/device-management.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-device-management',
+  templateUrl: './device-management.component.html',
+  styleUrls: ['./device-management.component.scss']
+})
+export class DeviceManagementComponent implements OnInit {
+  @Input()deviceName:string;
+  @Input()deviceId:number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
