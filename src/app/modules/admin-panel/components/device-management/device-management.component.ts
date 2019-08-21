d10909f514975ca15664e2779943e3927c740175
souravmondal13 <souravcristiano502@gmail.com>
2019-08-21 21:33:47 +0530

    modified

--- src/app/modules/admin-panel/components/device-management/device-management.component.ts
+++ src/app/modules/admin-panel/components/device-management/device-management.component.ts
@@ -3,6 +3,7 @@ import { Device, DeviceMonitor } from '../../model/customermodel';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { DeviceActiveRendererComponent } from 'src/app/modules/shared/components/device-active-renderer/device-active-renderer.component';
 import { DeviceHealthRendererComponent } from 'src/app/modules/shared/components/device-health-renderer/device-health-renderer.component';
+import { AdminPanelMainService } from '../../admin-panel-main.service';
 
 
 @Component({
@@ -14,17 +15,19 @@ export class DeviceManagementComponent implements OnInit {
   @Input()deviceName:string;
   @Input()deviceId:number;
   @Input()deviceHealth: DeviceMonitor[] = []
-
+  private defaultcolDefs;
   private deviceMonitorGridApi;
   private deviceMonitorColumnApi;
-  constructor(private spinner: NgxSpinnerService) { }
+  constructor(private spinner: NgxSpinnerService, private adminService: AdminPanelMainService) {
+    this.defaultcolDefs = { resizable: true }
+   }
 
   columnDefs = [
-    { headerName: 'Device Name', field:'device_name', sortable:true },
-    { headerName: 'Device MAC Address', field: 'device_mac' },
+    { headerName: 'Device Name', field:'device_name', sortable:true, filter:'agTextColumnFilter' },
+    { headerName: 'Device MAC Address', field: 'device_mac', filter:'agTextColumnFilter' },
     { headerName: 'Active', field:'device_activated', sortable:true, cellRenderer:'activeRenderer'},
     { headerName: 'Health', field:'device_health', cellRenderer:'healthRenderer'},
-    { headerName: 'Last Heart Beat', field:'device_last_heartbeat'},
+    { headerName: 'Last Heart Beat', field:'device_last_heartbeat',  sortable:true},
     { headerName: 'Data Collection Frequency', field:'data_collection_frequency', editable:true},
     { headerName: 'Data Sending Frequency', field:'data_sending_frequency', editable:true, resizable:true}
   ];
@@ -37,17 +40,28 @@ export class DeviceManagementComponent implements OnInit {
     { device_name:'dvc_005', device_mac:'00:e0:4a:0a:ae:e3', device_activated:false, device_health:'', device_last_heartbeat:'' },
     { device_name:'dvc_006', device_mac:'00:e0:4a:0a:ae:e3', device_activated:true, device_health:'ok', device_last_heartbeat:'20/08/2019' },
   ]
-
+  
   frameworkComponents = {
     activeRenderer: DeviceActiveRendererComponent,
     healthRenderer: DeviceHealthRendererComponent
   }
 
   ngOnInit() {
+    this.adminService.getDeviceHealth().subscribe(
+      (data) => {
+        this.spinner.show();
+        console.log(data)
+        this.spinner.hide();
+      },
+      (error) => {
+        console.log(error)
+      }
+    );
   }
 
   onDeviceGridReady(params){
     this.deviceMonitorGridApi = params.api;
     this.deviceMonitorColumnApi = params.columnApi;
+    this.deviceMonitorGridApi.sizeColumnsToFit()
   }
 }
