6557680b93c838ed6bbc745de253c12cb2a4c405
souravmondal13 <souravcristiano502@gmail.com>
2019-08-26 16:11:59 +0530

    added elements to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MaterialModule } from '../material/material.module';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
