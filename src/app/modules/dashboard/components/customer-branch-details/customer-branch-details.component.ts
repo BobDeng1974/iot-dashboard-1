724818bcc0f2cb096954294c9d9e05face87b517
shatanu1998a <shantanurong44@gmail.com>
2019-08-26 16:11:39 +0530

    add two component

---
+++ src/app/modules/dashboard/components/customer-branch-details/customer-branch-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-branch-details',
+  templateUrl: './customer-branch-details.component.html',
+  styleUrls: ['./customer-branch-details.component.scss']
+})
+export class CustomerBranchDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/dashboard/components/customer-branch-device-details/customer-branch-device-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-branch-device-details',
+  templateUrl: './customer-branch-device-details.component.html',
+  styleUrls: ['./customer-branch-device-details.component.scss']
+})
+export class CustomerBranchDeviceDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
