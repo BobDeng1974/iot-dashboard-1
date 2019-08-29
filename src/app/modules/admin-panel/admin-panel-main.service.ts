222a6789a2cd3e4a87af28ceeb223940b2e29429
shatanu1998a <shantanurong44@gmail.com>
2019-08-29 12:22:19 +0530

    create vendor module

--- src/app/modules/admin-panel/admin-panel-main.service.ts
+++ src/app/modules/admin-panel/admin-panel-main.service.ts
@@ -12,54 +12,6 @@ import { environment } from '../../../environments/environment';
 })
 export class AdminPanelMainService {
 
-  // private createCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/create/";
-  // private updateCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/update/";
-  // private getAllCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/getall/?format=json";
-  // private getACustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/getacustomer/";
-  // private getdomainUrl = "http://35.200.162.115:8006/qiidomain/get/";
-  // //////////////////////////////////////////////////////////////////////////
-  // private getCustomerBranchUrl = "http://34.93.221.249:8000/api/qubematics/customer/getbranch/";
-  // private getCustomerandIdUrl = "http://34.93.221.249:8000/api/qubematics/customer/getcustomerandid/";
-  // //////////////////////////////////////////////////////////////////////////
-  // // private createDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/create/";
-  // // private updateDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/update/";
-  // // private getAllDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/getall/?format=json";
-  // // private deviceAliveUrl = "http://192.168.0.103:8001/api/qubematics/device/alive/";
-  // // private assignDevice = "http://192.168.0.103:8001/api/qubematics/device/assign/";
-  // private createDeviceUrl = "http://34.93.221.249:8002/api/qubematics/device/create/";
-  // private updateDeviceUrl = "http://34.93.221.249:8002/api/qubematics/device/update/";
-  // private getAllDeviceUrl = "http://34.93.221.249:8002/api/qubematics/device/getall/?format=json";
-  // private getAssignInfoUrl = "http://34.93.221.249:8002/api/qubematics/device/assignInfo/";
-  // private deviceAliveUrl = "http://34.93.221.249:8002/api/qubematics/device/alive/";
-  // private assignDevice = "http://34.93.221.249:8002/api/qubematics/device/assign/";
-  // private getDeviceHealthUrl = "http://34.93.221.249:8002/api/qubematics/device/getdevicehealth/?format=json";
-  // private updateFreqUrl = "http://34.93.221.249:8002/api/qubematics/device/updatefreq/";
-  // private getADevice =  "http://34.93.221.249:8002/api/qubematics/device/get/";
-  // // private createCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/create/";
-  // // private updateCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/update/";
-  // // private getAllCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/getall/?format=json";
-  // // private getACustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/getacustomer/";
-  // // private getdomainUrl = "http://35.200.162.115:8006/qiidomain/get/";
-  // // private createDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/create/";
-  // // private updateDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/update/";
-  // // private getAllDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/getall/?format=json";
-  // // private deviceAliveUrl = "http://192.168.0.103:8001/api/qubematics/device/alive/";
-  // // private assignDevice = "http://192.168.0.103:8001/api/qubematics/device/assign/";
-  // // // private createDeviceUrl = "http://192.168.0.11:8001/api/qubematics/device/create/";
-  // // private updateDeviceUrl = "http://192.168.0.11:8001/api/qubematics/device/update/";
-  // // private getAllDeviceUrl = "http://192.168.0.11:8001/api/qubematics/device/getall/?format=json";
-  // // private deviceAliveUrl = "http://192.168.0.11:8001/api/qubematics/device/alive/";
-  // // private assignDevice = "http://192.168.0.11:8001/api/qubematics/device/assign/";
-
-  // private getAllVendorUrl = "http://34.93.221.249:8001/api/qubematics/vendor/getall/?format=json";
-  // private postVendorUrl = "http://34.93.221.249:8001/api/qubematics/vendor/create/?format=json";
-  // private vendorUpdateUrl = "http://34.93.221.249:8001/api/qubematics/vendor/update/";
-  // private getAvendorUrl = "http://34.93.221.249:8001/api/qubematics/vendor/getavendor/";
-  // private getVendorNameIdUrl = "http://34.93.221.249:8001/api/qubematics/vendor/vendornameid/?format=json";
-  // private createVendorManage = "http://34.93.221.249:8001/api/qubematics/vendor/vendormanage/";
-  // private assignMentHistory = "http://34.93.221.249:8001/api/qubematics/vendor/gethistory/";
-  // private graphUrl = "http://127.0.0.1:8086/query?db=sensor&q=select*from";
-
   constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
 
   createCustomer(form: Customer){
@@ -217,8 +169,8 @@ export class AdminPanelMainService {
       break;
     }
   }
-  getGraphData() : Observable<any> {
-    console.log("calling : " + environment.graphUrl+' sensor');
-    return this.http.get<any>(environment.graphUrl+' sensor');
-  }
+  // getGraphData() : Observable<any> {
+  //   console.log("calling : " + environment.graphUrl+' sensor');
+  //   return this.http.get<any>(environment.graphUrl+' sensor');
+  // }
 }
