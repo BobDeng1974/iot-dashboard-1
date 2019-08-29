ca895aafb0ff486891fc8fe33d33c26604b957f5
souravmondal13 <souravcristiano502@gmail.com>
2019-08-30 04:56:17 +0530

    added urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,11 +14,6 @@ export const environment = {
    getCustomerBranchUrl : "http://34.93.22.112:8000/api/qubematics/customer/getbranch/",
    getCustomerandIdUrl : "http://34.93.22.112:8000/api/qubematics/customer/getcustomerandid/",
   //////////////////////////////////////////////////////////////////////////
-  // private createDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/create/";
-  // private updateDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/update/";
-  // private getAllDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/getall/?format=json";
-  // private deviceAliveUrl = "http://192.168.0.103:8001/api/qubematics/device/alive/";
-  // private assignDevice = "http://192.168.0.103:8001/api/qubematics/device/assign/";
    createDeviceUrl : "http://34.93.22.112:8002/api/qubematics/device/create/",
    updateDeviceUrl : "http://34.93.22.112:8002/api/qubematics/device/update/",
    getAllDeviceUrl : "http://34.93.22.112:8002/api/qubematics/device/getall/?format=json",
@@ -28,21 +23,6 @@ export const environment = {
    getDeviceHealthUrl : "http://34.93.22.112:8002/api/qubematics/device/getdevicehealth/?format=json",
    updateFreqUrl : "http://34.93.22.112:8002/api/qubematics/device/updatefreq/",
    getADevice :  "http://34.93.22.112:8002/api/qubematics/device/get/",
-  // private createCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/create/";
-  // private updateCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/update/";
-  // private getAllCustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/getall/?format=json";
-  // private getACustomerUrl = "http://34.93.221.249:8000/api/qubematics/customer/getacustomer/";
-  // private getdomainUrl = "http://35.200.162.115:8006/qiidomain/get/";
-  // private createDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/create/";
-  // private updateDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/update/";
-  // private getAllDeviceUrl = "http://192.168.0.103:8001/api/qubematics/device/getall/?format=json";
-  // private deviceAliveUrl = "http://192.168.0.103:8001/api/qubematics/device/alive/";
-  // private assignDevice = "http://192.168.0.103:8001/api/qubematics/device/assign/";
-  // // private createDeviceUrl = "http://192.168.0.11:8001/api/qubematics/device/create/";
-  // private updateDeviceUrl = "http://192.168.0.11:8001/api/qubematics/device/update/";
-  // private getAllDeviceUrl = "http://192.168.0.11:8001/api/qubematics/device/getall/?format=json";
-  // private deviceAliveUrl = "http://192.168.0.11:8001/api/qubematics/device/alive/";
-  // private assignDevice = "http://192.168.0.11:8001/api/qubematics/device/assign/";
 
    getAllVendorUrl : "http://34.93.22.112:8001/api/qubematics/vendor/getall/?format=json",
    postVendorUrl : "http://34.93.22.112:8001/api/qubematics/vendor/create/?format=json",
@@ -51,8 +31,9 @@ export const environment = {
    getVendorNameIdUrl : "http://34.93.22.112:8001/api/qubematics/vendor/vendornameid/?format=json",
    createVendorManage : "http://34.93.22.112:8001/api/qubematics/vendor/vendormanage/",
    assignMentHistory : "http://34.93.22.112:8001/api/qubematics/vendor/gethistory/",
+   customerManageUrl : "http://127.0.0.1:8000/api/qubematics/vendor/customerassign/",
    //graphUrl : "http://127.22.112:8086/query?db=sensor&q=select*from",
-   dataGraphUrl : "http://34.93.150.203:8086/query?db=sensorReading&q=select * from DeviceSensorReading",
+   dataGraphUrl : "http://34.93.221.249:8086/query?db=sensorReading&q=select * from DeviceSensorReading",
    //login url
    postLoginUrl : "http://192.168.0.14:8003/api/qubematics/authuser/"
 };
