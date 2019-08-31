79cfd28b4d333728f60c478449c68621dd37cd1a
shatanu1998a <shantanurong44@gmail.com>
2019-08-31 12:55:49 +0530

    ok

--- src/app/modules/admin-panel/components/view-vendor-customer/view-vendor-customer.component.ts
+++ src/app/modules/admin-panel/components/view-vendor-customer/view-vendor-customer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AdminPanelMainService } from '../../admin-panel-main.service';
+import { Vendor } from '../../model/vendormodel';
+import { Customer } from '../../model/customermodel';
 
 @Component({
   selector: 'app-view-vendor-customer',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewVendorCustomerComponent implements OnInit {
 
-  constructor() { }
+  AdvanceSearchForm : FormGroup;
+  allVendor : Vendor[];
+  allCustomer : Customer[];
+
+  constructor(private fb : FormBuilder, public dialogRef : MatDialogRef<ViewVendorCustomerComponent>,private spinner : NgxSpinnerService, private adminMainService : AdminPanelMainService) { }
 
   ngOnInit() {
+    this.spinner.show();
+    this.adminMainService.getAllVendor().subscribe(
+      (data) => {
+        console.log(data);
+        this.allVendor = data;
+        this.adminMainService.getAllCustomer().subscribe(
+          (data) => {
+            this.allCustomer = data;
+            console.log(data);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+        this.spinner.hide();
+      },
+      (error) => {
+        console.error(error);
+        this.spinner.hide();
+      }
+    );
+    this.AdvanceSearchForm = this.fb.group({
+      vendor : '',
+      customer : '',
+    });
+  }
+
+  CancelOperation() {
+    this.dialogRef.close();
+  }
+
+  onSubmit(form) {
+
   }
 
 }
