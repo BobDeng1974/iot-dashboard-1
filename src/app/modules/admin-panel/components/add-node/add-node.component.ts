898d53763ed266b646dd7afc9090c7d5700f9f5f
souravmondal13 <souravcristiano502@gmail.com>
2019-10-14 18:58:28 +0530

    adder add node form

---
+++ src/app/modules/admin-panel/components/add-node/add-node.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'app-add-node',
+  templateUrl: './add-node.component.html',
+  styleUrls: ['./add-node.component.scss']
+})
+export class AddNodeComponent implements OnInit {
+  nodeForm: FormGroup;
+  constructor(private fb: FormBuilder, private dialogref : MatDialogRef<AddNodeComponent>) { }
+
+  ngOnInit() {
+    this.nodeForm = this.fb.group({
+      uid:'',
+      data_collection_frequency:'',
+      data_sending_frequency:''
+    })
+  }
+
+  closeDialog(){
+    this.dialogref.close()
+  }
+
+  saveNode(form) {
+    
+  }
+}
