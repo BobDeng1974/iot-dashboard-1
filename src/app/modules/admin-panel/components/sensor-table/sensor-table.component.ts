d82d649e15ccc90f7da8c4abd3bdb2de62d2875c
souravmondal13 <souravcristiano502@gmail.com>
2019-10-16 14:39:19 +0530

    added sensor table

--- src/app/modules/admin-panel/components/sensor-table/sensor-table.component.ts
+++ src/app/modules/admin-panel/components/sensor-table/sensor-table.component.ts
@@ -8,6 +8,7 @@ import { AdminPanelMainService } from '../../admin-panel-main.service';
 })
 export class SensorTableComponent implements OnInit {
   @Output() buttonClicked = new EventEmitter<number>();
+  displayedCloumns : string[] = ['select', 'sensor_type', 'sensor_model', 'sensor_make', 'sensor_status', 'sensor_threshold_max', 'sensor_threshold_min']
   constructor(private adminPanelService: AdminPanelMainService) { }
 
   ngOnInit() {
