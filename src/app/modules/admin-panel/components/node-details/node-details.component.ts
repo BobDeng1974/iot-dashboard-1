c7276402a4e443200561f04fc269b13277b9d5de
souravmondal13 <souravcristiano502@gmail.com>
2019-10-18 19:56:07 +0530

    fixed node details

--- src/app/modules/admin-panel/components/node-details/node-details.component.ts
+++ src/app/modules/admin-panel/components/node-details/node-details.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NullValueComponent } from 'src/app/modules/shared/components/null-value/null-value.component';
 import { sensor } from '../../model/gateway';
+import { AdminPanelMainService } from '../../admin-panel-main.service';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { MatSnackBar } from '@angular/material';
+import { SuccessSnackberComponent } from 'src/app/modules/shared/components/success-snackber/success-snackber.component';
 
 @Component({
   selector: 'app-node-details',
@@ -13,8 +17,9 @@ export class NodeDetailsComponent implements OnInit {
   nullvalueFrameworkComponents: any;
   rowData : sensor[] = [];
   nodeDetailGridApi;
-  nodeDetailColumnApi; 
-  constructor() { }
+  nodeDetailColumnApi;
+  @Input() sensors : sensor[] = []; 
+  constructor(private adminPanelService : AdminPanelMainService, private spinner : NgxSpinnerService, private _snackBar : MatSnackBar) { }
 
   ngOnInit() {
     this.nullvalueFrameworkComponents = {
@@ -22,9 +27,17 @@ export class NodeDetailsComponent implements OnInit {
     };
     this.sensorColumns = [
       {headerName : 'Model', field:'sensor_model'},
-      {headerName : 'Maximum', field:'sensor_threshold_max', editable:true, sortable:true, cellEditor:'nullvalueEditor'},
-      {headerName : 'Minimum', field:'sensor_threshold_min', editable:true, sortable:true, cellEditor:'nullvalueEditor'},
+      {headerName : 'Maximum', field:'sensor_threshold_max', editable:true, sortable:true, valueParser: numberParser},
+      {headerName : 'Minimum', field:'sensor_threshold_min', editable:true, sortable:true, valueParser: numberParser},
     ]
+    this.rowData = [];
+  }
+  ngOnChanges(){
+    if (this.sensors.length > 0) {
+      this.rowData = this.sensors;  
+    }else{
+      this.rowData = []
+    }
   }
   onNodeDetailGridReady(params) {
     this.nodeDetailGridApi = params.api;
@@ -32,4 +45,27 @@ export class NodeDetailsComponent implements OnInit {
     this.nodeDetailGridApi.sizeColumnsToFit();
   }
 
+  onCellValueChange(params){
+    let sensor : sensor = params.data;
+    this.spinner.show();
+    this.adminPanelService.updateSensor(sensor).subscribe(
+      (data) => {
+        this.spinner.hide();
+        if (data === "001") {
+          this._snackBar.openFromComponent(SuccessSnackberComponent, {data:'Successfully updated sensor', duration:3000});
+        } else {
+          this.adminPanelService.getError(data)
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+    console.log(params);
+  }
+
+}
+
+function numberParser(params){
+  return Number(params.newValue)
 }
