0d2d178db3eba4b70b9de14b2a5334177820d231
souravmondal13 <souravcristiano502@gmail.com>
2019-10-24 18:59:26 +0530

    added bottom nav bar

---
+++ src/app/common/mobile-bottom-nav-bar/mobile-bottom-nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-bottom-nav-bar',
+  templateUrl: './mobile-bottom-nav-bar.component.html',
+  styleUrls: ['./mobile-bottom-nav-bar.component.scss']
+})
+export class MobileBottomNavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
