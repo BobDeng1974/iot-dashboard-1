9b14fe3e49dfaa71873bf36473b72f5eeffb3a12
souravmondal13 <souravcristiano502@gmail.com>
2019-10-24 18:58:27 +0530

    added main route

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardMainComponent } from './pages/dashboard-main/dashboard-main.component';
+import { MobileDashboardMainComponent } from './pages/mobile-dashboard-main/mobile-dashboard-main.component';
 
 const routes:Routes = [
-  { path:"dashboard", component:DashboardMainComponent}
+  { path:"dashboard", component:DashboardMainComponent},
+  { path:"mobile-dashboard", component:MobileDashboardMainComponent }
 ]
 
 @NgModule({
